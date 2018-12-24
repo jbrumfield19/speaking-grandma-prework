39341d2a592f5d8e63db0d8fc620d8513f748388
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-24 00:10:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,10 @@ def speak_to_grandma
   if phrase== "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   end
-  elsif phrase == String.upcase
+  if phrase == String.upcase
     return "NO, NOT SINCE 1938"
   end
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
- end
  end
